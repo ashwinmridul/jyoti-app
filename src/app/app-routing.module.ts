58f67a7ce32e7aab6820231b51b1336ecef5e0da
Ashwin Mridul <ashwin.mridul1@myntra.com>
2022-06-19 21:34:27 +0530

    Routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataComponent } from './data/data.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'data', component: DataComponent }
 ];
 
